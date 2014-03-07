2a148c9eb65df815340dca551965345b337ff9b7
Filipe Arantes Fernandes <filran@gmail.com>
2014-03-07 03:38:36 -0300

    Implementada edição do post-it
    So falta resolver o click do NP

--- scripts/plugins/expand/expand.js
+++ scripts/plugins/expand/expand.js
@@ -15,7 +15,7 @@
         $(".backgroundexpand").css({"z-index":"1","opacity":0}).addClass("com_fundo");
         $(".backgroundexpand").animate({"opacity":1},time);
 
-        if( tipo=="postit" || tipo=="newpostit" ){
+        if( tipo=="postit" || tipo=="newpostit"){
             //create elements
             $("<div bigpostit-id='' class='bigpostit'></div>").appendTo(".backgroundexpand");
 
@@ -35,17 +35,48 @@
 
             $(".bigpostit")
                 .append("<div id='autor'>Autor:"+autor+"</div>")
-                .append("<div id='fechar'><a href='#'>X</a></div>") //Botão Fechar
-                .append("<p id='conteudopostit' class='write' contenteditable='true'>"+content+"</p>");
-        
-       
-            //NEWPOSTIT==============================================
-            if( tipo=="newpostit" ){                     
-                $("#teclado").css("display","block");
-            }                
+                .append("<div><a href='#' id='fechar'><img src='imagens/icones/right.png'/></a></div>") //Botão Fechar
+                .append("<div><a href='#' id='abrirteclado'><img src='imagens/icones/keyboard.png'/></a></div>")
+                .append("<div id='wrapconteudopostit'><p id='conteudopostit' class='write' contenteditable='true'>"+content+"</p></div>");
 
-            //close
-            $("#fechar > a").click(function(){
+            $("#abrirteclado").click(function(){
+                tecladovisivel = $("#teclado").css("display");
+                if( tecladovisivel=="block" ){
+                    $("#teclado").css("display","none");
+                }else{
+                    $("#teclado").css("display","block");
+                }                
+            });
+
+            //CENTRALIZAR CONTEUDO DO POST-IT
+            centralizar_conteudopostit();
+            $("#conteudopostit")
+                .keydown(function(){
+                    centralizar_conteudopostit();
+                }).mouseout(function(){
+                    centralizar_conteudopostit();
+                }).trigger("click");
+
+            $("#keyboard > li").click(function(){
+                centralizar_conteudopostit();
+            });
+
+
+            if( tipo=="newpostit" ){
+                //Indicar local para digitar
+                texto_anotacao = "Sua anotação aqui";
+                conteudo_postit = $("#conteudopostit").text();
+                $("#conteudopostit")
+                    .html(texto_anotacao)
+                    .one("click",function(){
+                        $(this).html("");
+                    });                
+            }
+            
+            
+
+            //CLOSE
+            $("#fechar").click(function(){
                 $(selectorbig+".bigpostit").animate({"opacity":"0"},time, function(){
                     $(this).remove();     
                     $(".backgroundexpand").animate({"background-color":"rgba(0,0,0,0)"}).css({"z-index":"-1"},time,function(){
@@ -57,10 +88,10 @@
                     $(".backgroundexpand").remove();
                 });
 
-                if( tipo=="newpostit" ){
-                    $("#teclado").css("display","none");
-                    $("#container_keyboard").removeAttr("style");
-                }
+
+                $("#teclado").css("display","none");
+                $("#container_keyboard").removeAttr("style");
+        
 
                 //atualizar conteudo do postit
                 conteudo = $("#conteudopostit").text();
@@ -69,6 +100,7 @@
                 }else{
                     $(selector).html(conteudo);
                 }
+                
             });
             
         }else if(tipo=="area"){
@@ -107,4 +139,13 @@
             
     }    
 })(jQuery);
+
+
+//centralizar conteudopostit==================================
+function centralizar_conteudopostit(){
+    var altura_wrapconteudopostit = $("#wrapconteudopostit").height(); // T      100%
+    var altura_conteudopostit = $("#conteudopostit").height();         // P      x%      
+    var top_conteudopostit = Math.round( (((altura_wrapconteudopostit-altura_conteudopostit)/2)*100)/altura_wrapconteudopostit)-4+"%";
+    $("#conteudopostit").css("top",top_conteudopostit);
+}
 
